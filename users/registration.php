d514f1aadd6ccbee2e02f5e206c0c9a02c13297c
kundan kumarjha <kundanjha1076@gmail.com>
2020-02-15 01:39:17 +0530

    pusing to github

---
+++ users/registration.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+header('location:login.php');
+
+
+$con=mysqli_connect("localhost","root","");
+mysqli_select_db($con,'registration');
+$name = $_POST['user'];
+$pass = $_POST['password'];
+$s="select * from users where name = '$name'";
+$result= mysqli_query($con,$s);
+$num = mysqli_num_rows($result);
+if($num==1){
+    echo"Username Already Taken";
+}else{
+    $reg="insert into users(name,password) values('$name','$pass')";
+
+mysqli_query($con,$reg);
+    echo " Registration Suceesfull";
+
+}
+
+
+?>
